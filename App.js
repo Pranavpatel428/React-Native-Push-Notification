4ff6b6bf585c3e9ce1c5b472f885f2f30b6bf6a7
Pranav Patel <pranavpatel428@gmail.com>
2021-12-15 09:58:32 +0530

    Connecting to GraphQL

--- App.js
+++ App.js
@@ -2,20 +2,42 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Header = () => (
+  <View style={styles.header}>
+    <Text style={styles.headerText}>React Native Demo App</Text>
+  </View>
+)
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.contentContainer}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
-});
+  header: {
+    marginTop: 50,
+    alignItems: 'center',
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  headerText: {
+    marginBottom: 5,
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  contentContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 20
+  }
+})
+
+export default App
